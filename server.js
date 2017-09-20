c8867234c9fb0dd29a42a42f376d4e59977470c5
Catislin <caitlin.estrellita@gmail.com>
2017-09-20 15:12:43 -0700

    added new post form

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/front-page');
 var hb = require('express-handlebars');
 
 app.engine('handlebars', hb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var Post = mongoose.model('Post', {
+  title: String
+});
+
 app.get('/', function(req, res) {
-  res.send('Hello world!');
+  res.render('home');
+});
+
+app.get('/posts/:id', function (req, res) {
+  Post.findById(req.params.id).exec(function (err, post) {
+    res.render('show-post', {post: post});
+  });
+});
+
+app.get('/posts/new', function(req, res) {
+  res.render('new-post', {});
+});
+
+app.post('/posts', function (req, res) {
+  Post.create(req.body, function(err, rv) {
+    res.redirect('/posts/' + rv._id);
+  })
 });
 
 app.listen(3000, function() {
