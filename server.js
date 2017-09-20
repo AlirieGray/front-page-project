b1d1575648d6109c1dd8d05ba5baf3be218a0d46
Catislin <caitlin.estrellita@gmail.com>
2017-09-20 15:38:14 -0700

    added post model

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/front-page');
 var hb = require('express-handlebars');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/front-page');
+var bodyParser = require('body-parser');
 app.engine('handlebars', hb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true }));
+require('./controller/posts.js')(app);
 
-var Post = mongoose.model('Post', {
-  title: String
-});
 
 app.get('/', function(req, res) {
   res.render('home');
 });
 
-app.get('/posts/:id', function (req, res) {
-  Post.findById(req.params.id).exec(function (err, post) {
-    res.render('show-post', {post: post});
-  });
-});
-
 app.get('/posts/new', function(req, res) {
-  res.render('new-post', {});
-});
-
-app.post('/posts', function (req, res) {
-  Post.create(req.body, function(err, rv) {
-    res.redirect('/posts/' + rv._id);
-  })
+  res.render('new-post');
 });
 
 app.listen(3000, function() {
