680a85af2edde73d13b94bb6e143cc39d5b85f24
Catislin <caitlin.estrellita@gmail.com>
2017-09-25 00:37:31 -0700

    added sign up features

---
+++ controller/auth.js
@@ -0,0 +1,20 @@
+var bodyParser = require('body-parser');
+var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+
+module.exports = function(app) {
+  app.post('/sign-up', function(req, res, next) {
+    // create User and JWT
+    var user = new User(req.body);
+
+    user.save(function (err) {
+      if (err) {
+        return res.status(400).send({ err: err});
+      }
+      var token = jwt.sign({ id: user.id}, process.env.SECRET, { expiresIn: "60 days"});
+      // set the cookie
+      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true});
+      res.redirect('/');
+    })
+  });
+}
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
 var hb = require('express-handlebars');
+app.use(cookieParser);
 app.engine('handlebars', hb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 var mongoose = require('mongoose');
@@ -9,13 +11,19 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./controller/posts.js')(app);
 require('./controller/comments.js')(app);
+require('./controller/auth.js')(app);
 var Post = require('./models/post');
 var Comment = require('./models/comment');
+var User = require('./models/user');
 
 app.get('/', function(req, res) {
   Post.find(function(err, posts) {
     res.render('posts-index', {posts: posts});
   });
+});
+
+app.get('/sign-up', function(req, res, next) {
+  res.render('sign-up');
 });
 
 app.get('/posts/new', function(req, res) {
